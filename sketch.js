2162d8deb960a091f0ca5ca3061c7463c61d6a07
genebit <genebitara@gmail.com>
2021-06-20 13:09:36 +0800

    Created test.js, @sketch.js: Finished keyPressed function

--- sketch.js
+++ sketch.js
@@ -1,63 +1,41 @@
 
 
-let fps = 10;
-
-let xPosition = 100;
-let yPosition = 100;
-let direction = "RIGHT";
-let speed = 10;
+let fps = 20;
 
 function setup() {
   createCanvas(400, 400);
   frameRate(fps);
 }
-
-let xCor = [];
-let yCor = [];
+let direction;
 
 function draw() {
   background(225);
 
   translate(-15, -15)
   fill(26);
-  for (let i = 0; i < numSegments - 1; i++) {
-    line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
-  }
-  // rect(xPosition, yPosition, 30, 30);
-
-  // switch (direction) {
-  //   case "LEFT":
-  //     xPosition -= speed;
-  //     break;
-  //   case "RIGHT":
-  //     xPosition += speed;
-  //     break;
-  //   case "UP":
-  //     yPosition -= speed;
-  //     break;
-  //   case "DOWN":
-  //     yPosition += speed;
-  //     break;
-  // }
 }
 
 function keyPressed() {
   switch (keyCode) {
     case LEFT_ARROW:
-      print('Left');
-      direction = "LEFT";
+      if (direction !== 'RIGHT') {
+        direction = 'LEFT';
+      }
       break;
     case RIGHT_ARROW:
-      print('Right');
-      direction = "RIGHT";
+      if (direction !== 'LEFT') {
+        direction = 'RIGHT';
+      }
       break;
     case UP_ARROW:
-      print('Up');
-      direction = "UP";
+      if (direction !== 'DOWN') {
+      direction = 'UP';
+      }
       break;
     case DOWN_ARROW:
-      print('Down');
-      direction = "DOWN";
+      if (direction !== 'UP') {  
+        direction = 'DOWN';
+      }
       break;
   }
 }
---
+++ test.js
@@ -0,0 +1,12 @@
+/* 
+ * For testing purposes
+ */ 
+
+let segments = 5;
+
+let array = [];
+
+for (let index = 0; index < segments; index++) {
+    array.push(index);
+    console.log(array);
+}
