2d4e357b8e95215e2fd6773fab0f452c98f1a717
genebit <genebitara@gmail.com>
2021-07-16 13:23:14 +0800

    Finished coding the increase length of the snake

---
+++ test/_snakelength.py
@@ -0,0 +1,34 @@
+import pygame
+import sys
+
+window = pygame.display.set_mode((400, 400))
+
+snake = [None, None, None]
+
+def draw_snake():
+    snake_x = 300
+
+    for i in range(0, len(snake)):
+        
+        snake[i] = pygame.Rect(snake_x, window.get_height()/2, 30, 30)
+        snake_x -= 30
+        
+        snake[i] = pygame.draw.rect(window, (255, 255, 255), snake[i], 2)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                # Grow
+                snake.append(None)
+
+    window.fill((30, 30, 30))
+
+    draw_snake()
+
+    pygame.display.update()
+    
