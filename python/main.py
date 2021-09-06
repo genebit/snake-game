914002ff722636680278021445919c83ad723b94
genebit <genebitara@gmail.com>
2021-09-06 21:39:48 +0800

    Redoing snakegame code

--- python/main.py
+++ python/main.py
@@ -1,81 +1,95 @@
 import pygame
-import sys
 import random
 
-FPS = 8
-WINDOW_WIDTH = 400
-WINDOW_HEIGHT = 400
+import texts
 
 pygame.init()
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (26 ,26, 26)
+RED = (186, 45, 45)
+# Window Properties
+WINDOW = pygame.display.set_mode((500, 500))
 pygame.display.set_caption('Snake Game')
+CLOCK = pygame.time.Clock()
 
-clock = pygame.time.Clock()
+TEXT = texts.Text(WINDOW)
 
-snake = pygame.Rect(100, WINDOW_HEIGHT/2, 25, 25)
-x_update = 0
-y_update = 0
-movelength = 25
-direction = 'RIGHT'
+# Snake
+snake = pygame.Rect(250, 250, 25, 25)
 
+snake_x_update = 0
+snake_y_update = 0
+
+# Food
 x_grid = []
 y_grid = []
-for x in range(0, WINDOW_WIDTH, 25):
+
+for x in range(0, WINDOW.get_width(), 25):
     x_grid.append(x)
-    for y in range(0, WINDOW_HEIGHT, 25):
+    for y in range(0, WINDOW.get_height(), 25):
         y_grid.append(y)        
 
-if x_grid != [] and y_grid != []:
-    # Pick a random position
-    x_picker = random.randint(0, len(x_grid)-1)
-    y_picker = random.randint(0, len(y_grid)-1)
-    
-    food = pygame.Rect(x_grid[x_picker], y_grid[y_picker], 25, 25)
-    
-    print('Food is at:', food.x, food.y)
+def spawnFood(x_grid, y_grid):
+    food_x_picker = random.randint(0, len(x_grid)-1)
+    food_y_picker = random.randint(0, len(y_grid)-1)
+    return pygame.Rect(food_x_picker, food_y_picker, 25, 25);
 
-gameover = False
+food = pygame.Rect(spawnFood(x_grid, y_grid))
 
 def main():
+    game_over = False
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                sys.exit()
-
+                quit()
+            # Movement Input
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP and not direction == 'DOWN':
-                    direction = 'UP'
-                if event.key == pygame.K_DOWN and not direction == 'UP':
-                    direction = 'DOWN'
-                if event.key == pygame.K_LEFT and not direction == 'RIGHT':
-                    direction = 'LEFT'
-                if event.key == pygame.K_RIGHT and not direction == 'LEFT':
-                    direction = 'RIGHT'
+                global snake_x_update, snake_y_update
+                # Up
+                if event.key == pygame.K_w: 
+                    snake_x_update = 0
+                    snake_y_update = -25
+                # Left
+                if event.key == pygame.K_a: 
+                    snake_x_update = -25
+                    snake_y_update = 0
+                # Down
+                if event.key == pygame.K_s: 
+                    snake_x_update = 0
+                    snake_y_update = 25
+                # Right
+                if event.key == pygame.K_d: 
+                    snake_x_update = 25
+                    snake_y_update = 0
+                #----------------------
+                # Reset
+                if event.key == pygame.K_r:
+                    snake.x = 250
+                    snake.y = 250
+                    game_over = False 
         
-        if direction == 'UP':
-            x_update = 0
-            y_update = -movelength
-        if direction == 'DOWN':
-            x_update = 0
-            y_update = movelength
-        if direction == 'LEFT':
-            x_update = -movelength
-            y_update = 0
-        if direction == 'RIGHT':
-            x_update = movelength
-            y_update = 0
-        
-        window.fill((26, 26, 26))
-        
-        snake.x += x_update
-        snake.y += y_update
-        pygame.draw.rect(window, (255, 255, 255), snake, 2)
-        
-        pygame.draw.rect(window, (201, 62, 62), food, 2)
-        
-        clock.tick(FPS)
-        pygame.display.update()
+        if not game_over:
+            snake.x += snake_x_update
+            snake.y += snake_y_update
+
+            WINDOW.fill(BLACK)
+            pygame.draw.rect(WINDOW, WHITE, snake, 2)
+            
+            pygame.draw.rect(WINDOW, RED, food, 2)
+
+            # Border Snake Collision Game Over
+            if (snake.x < 0 or snake.x > WINDOW.get_width() or 
+                snake.y < 0 or snake.y > WINDOW.get_height()):
+                TEXT.show_gameover(WINDOW)
+                game_over = True
+            
+            pygame.display.update()
+            CLOCK.tick(15)
 
 if __name__ == '__main__':
+    print('Game is Running...')
     main()
